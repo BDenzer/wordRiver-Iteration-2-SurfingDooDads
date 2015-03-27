5d4602cfae8d3d24920b2990c8c75edb7b280d95
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-03-27 18:45:27 -0500

    Figured out how to properly add to the database!

--- server/api/user/user.model.spec.js
+++ server/api/user/user.model.spec.js
@@ -18,10 +18,24 @@ var teacher = new User({
   name: 'Maryann Emerson',
   email: 'maryannemerson@puria.com',
   password: 'deserunt',
+  "_id" : "551072458e97142166a71900",
   studentList: [
     "551072458e97142166a71951"
    // "551072458e97142166a71952",
    // "551072458e97142166a71954"
+  ]
+});
+
+var teacher2 = new User({
+  provider: 'local',
+  name: 'TMaryann Emerson',
+  email: 'Tmaryannemerson@puria.com',
+  password: 'Tdeserunt',
+  "_id" : "1551072458e97142166a7190",
+  studentList: [
+    "151072458e97142166a71951"
+    // "551072458e97142166a71952",
+    // "551072458e97142166a71954"
   ]
 });
 
@@ -91,7 +105,7 @@ describe('User Model on empty database', function() {
     user.save(function(err) {
       should.exist(err);
       done();
-    });5
+    });
   });
 
   it("should authenticate user if password is valid", function() {
@@ -103,25 +117,77 @@ describe('User Model on empty database', function() {
   });
 });
 
+//var counter;
+//counter = 0;
 describe('User Model with populated database', function() {
-  before(function(done) {
-    teacher.save();
-    student1.save();
-    done();
+  beforeEach(function(done) {
+    //if (counter === 0) {
+    //  teacher.save();
+    //}
+    //if (counter === 1) {
+    //  teacher2.save();
+    //}
+    //user.save(function() {
+    //  var userDup = new User(user);
+    //  userDup.save(function(err) {
+    //    should.exist(err);
+    //    done();
+    //  });
+    //});
+    User.create(teacher, function(err, user) {
+      console.log("Adding user to database");
+      if (err) console.log("Error in User.create!");
+      User.find({}, function(err, users) {
+        if (err) console.log("Error in adding!");
+        console.log("Things in database: " + users.length);
+        done();
+      });
+    });
+    //teacher.save(function() { // Try taking out callback when working
+    //  console.log("Added teacher to database.");
+    //  User.find({}, function(err, users) {
+    //    if (err) console.log("Error in adding!");
+    //    console.log("Things in database: " + users.length);
+    //    //done();
+    //  });
+    //  //done();
+    //});
+    //console.log("outside save callback");
+    //done();
+
+
+    //student1.save();
+    //console.log("Added teacher to database.");
+    //counter += 1;
+    //done();
   });
 
   afterEach(function(done) {
     // Clears the User database
-    User.remove().exec().then(function() {
-      done();
+    //User.remove().exec().then(function() {
+    //  console.log("Database cleared");
+    //  done();
+    //});
+    //console.log("outside clear callback");
+    User.remove({}, function(err, something) {
+      console.log("Clearing database");
+      User.find({}, function(err, users) {
+        console.log("Things in database: " + users.length);
+        done();
+      });
     });
+    //done();
   });
 
   it('should be one teacher in database', function(done) {
     User.find({}, function(err, users) {
-      users.should.have.length(1);
+      console.log("HEREs first Teacher");
+      console.log(users[0]);
+      //users.should.have.length(1);
       done();
     });
+    //console.log("outside first teacher callback");
+    //done();
   });
 
   //it('teacher should have a student', function(done) {
@@ -134,29 +200,47 @@ describe('User Model with populated database', function() {
   //  done();
   //});
 
-  it('should return all of a teachers students', function(done) {
-    //User.find({name: 'Maryann Emerson'}, function(err, user) {
-    //  console.log(user);
-    //}).lean();
-    //var ourTeacher;
-    //ourTeacher = User.findbyId({name: 'Maryann Emerson'}).lean();
-    User.find({name: 'Maryann Emerson'}, function (err, user) {
-      //user.tileTags.push({"tagName": tagName, "packType": packType});
-      //user.save(function(err) {
-      //  if (err) return validationError(res, err);
-      //  res.send(200);
-      //});
-
-      console.log("Hello!");
-      console.log(user);
+  it('should be one teacher in databasePART2', function(done) {
+    User.find({}, function(err, users) {
+      console.log("HEREs second Teacher");
+      console.log(users[0]);
+      //users.should.have.length(1);
+      done();
     });
+    //console.log("outside 2nd teacher callback");
+    //done();
+  });
+
+  //it('should return all of a teachers students', function(done) {
+  //  User.find({}, function(err, users) {
+  //    console.log("HERES OUR TEACH");
+  //    console.log(users[0]);
+  //    //if (err) console.log("AHHHHH Error");
+  //    //else {
+  //    //  console.log("Here's our teacher: " + user.json());
+  //    //};
+  //    done();
+  //  });
+  //});
+    //var ourTeacher;
+    //ourTeacher = User.find({name: 'Maryann Emerson'}).name;
+    //console.log("Here's our teacher " + ourTeacher);
+    //User.find({name: 'Maryann Emerson'}, function (err, user) {
+    //  //user.tileTags.push({"tagName": tagName, "packType": packType});
+    //  //user.save(function(err) {
+    //  //  if (err) return validationError(res, err);
+    //  //  res.send(200);
+    //  //});
+    //
+    //  console.log("Hello!");
+    //  console.log(user);
+    //});
     //console.log(ourTeacher);
     //User.find({}, function(err, users) {
     //  users.should.have.length(1);
     //  done();
     //});
-    done();
-  });
+    //done();
   //
   //it('should return all of a teachers students', function(done) {
   //  var ourTeacher;
